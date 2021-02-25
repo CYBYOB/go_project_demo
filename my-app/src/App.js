c024b48544e5e58c27b3468d32f7fe2e8e2ea917
CYBYOB <1520112971@qq.com>
2021-02-25 12:06:45 +0800

    完成HomeHome、ClassRoom组件与路由的简单编码

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,12 @@
 
 import React, {Component} from 'react';
+import {
+    BrowserRouter as Router,
+    Route
+} from 'react-router-dom';
 
+import Home from './component/Home';
+import ClassRoom from './component/ClassRoom';
 
 class App extends Component {
     constructor(props) {
@@ -9,9 +15,10 @@ class App extends Component {
 
     render() {
         return (
-            <>
-            我是App
-            </>
+            <Router>
+                <Route exact path="/" component={Home} />
+                <Route path="/classRoom/:classRoomID" component={ClassRoom} />
+            </Router>
         )
     }
 }
---
+++ my-app/src/component/ClassRoom/index.js
@@ -0,0 +1,16 @@
+
+import React, {Component} from 'react';
+
+class ClassRoom extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>我是ClassRoom</div>
+        )
+    }
+}
+
+export default ClassRoom;
