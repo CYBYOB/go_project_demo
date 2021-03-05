4f77dfa922bfe2a3a94f43dcb1e4a8ed5cae858f
CYBYOB <1520112971@qq.com>
2021-03-05 19:46:44 +0800

    优化App.js，体现 程序 = 数据结构 + 算法

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,6 +9,15 @@ import ClassRoomEdit from './component/ClassRoomEdit';
 import Home from './component/Home';
 import ClassRoom from './component/ClassRoom';
 
+// （体现 程序 = 数据结构 + 算法）。这里放数据结构，render放算法
+const routeList = [
+    // 需求很明确，Route 只会传入 path 和 component 2个属性，所以这里用 数组[p, c] 代替 对象{path: p, component: c}
+    ["/", Home],
+    ["/classRoom/:classRoomID", ClassRoom],
+    // 将 edit 变更为 post 会更好？？
+    ["/classRoom/:classRoomID/edit", ClassRoomEdit]
+];
+
 class App extends Component {
     constructor(props) {
         super(props);
@@ -18,10 +27,12 @@ class App extends Component {
         return (
             <Router>
                 {/* 优点：通过类RESTful API风格的url快速区别不同的页面路径值 */}
-                <Route exact path="/" component={Home} />
-                <Route exact path="/classRoom/:classRoomID" component={ClassRoom} />
-                {/* 将 edit 变更为 post 会更好？ */}
-                <Route exact path="/classRoom/:classRoomID/edit" component={ClassRoomEdit} />
+                {
+                    routeList.map(item => {
+                        const [path, component] = item;
+                        return <Route exact key={path} path={path} component={component} />
+                    })
+                }
             </Router>
         )
     }
